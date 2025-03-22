be0742d735f3e992e2877ecf5565983fd4019ee0
Agah Berkin Güler <yedekguler@gmail.com>
2025-03-22 19:55:41 +0300

    create custom search button

---
+++ RoomNow/Screens/Search/View/SearchOptionButton.swift
@@ -0,0 +1,33 @@
+//
+//  SearchOptionButton.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 22.03.2025.
+//
+
+import UIKit
+
+class SearchOptionButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+
+    private func setupUI() {
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.setTitleColor(.black, for: .normal)
+        self.contentHorizontalAlignment = .left
+        self.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        self.backgroundColor = .white
+    }
+
+
+}
