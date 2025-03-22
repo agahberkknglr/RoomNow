ef44d1dcbd9087d751c17347c1a683ddab4284a5
Agah Berkin Güler <yedekguler@gmail.com>
2025-03-22 19:56:30 +0300

    custom search button added search screen

--- RoomNow/Screens/Search/View/SearchVC.swift
+++ RoomNow/Screens/Search/View/SearchVC.swift
@@ -9,9 +9,14 @@ import UIKit
 
 protocol SearchVCProtocol: AnyObject {
     func configureVC()
+    func setupSearchButtons()
 }
 
-final class SearchVC: UIViewController {
+final class SearchVC: UIViewController{
+    
+    private var destinationButton = SearchOptionButton()
+    private var dateButton = SearchOptionButton()
+    private var roomButton = SearchOptionButton()
     
     private let viewModel = SearchVM()
 
@@ -26,5 +31,51 @@ extension SearchVC: SearchVCProtocol {
     
     func configureVC() {
         view.backgroundColor = .brown
+    }
+    
+    func setupSearchButtons() {
+        destinationButton.setTitle("Select Destination", for: .normal)
+        dateButton.setTitle("Select Dates", for: .normal)
+        roomButton.setTitle("Select Rooms & Guests", for: .normal)
+        
+        destinationButton.addTarget(self, action: #selector(openDestinationSheet), for: .touchUpInside)
+        dateButton.addTarget(self, action: #selector(openDateSheet), for: .touchUpInside)
+        roomButton.addTarget(self, action: #selector(openRoomSheet), for: .touchUpInside)
+        
+        let stackView = UIStackView(arrangedSubviews: [destinationButton, dateButton, roomButton])
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.distribution = .fillEqually
+
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            destinationButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc private func openDestinationSheet() {
+        presentBottomSheet(with: DestinationVC(), detents: [.medium(), .large()])
+    }
+
+    @objc private func openDateSheet() {
+        presentBottomSheet(with: DestinationVC(), detents: [.medium()])
+    }
+
+    @objc private func openRoomSheet() {
+        presentBottomSheet(with: DestinationVC(), detents: [.medium(), .large()])
+    }
+
+    private func presentBottomSheet(with viewController: UIViewController, detents: [UISheetPresentationController.Detent]) {
+        let navController = UINavigationController(rootViewController: viewController)
+        if let sheet = navController.sheetPresentationController {
+            sheet.detents = detents
+            sheet.prefersGrabberVisible = true
+            sheet.preferredCornerRadius = 16
+        }
+        present(navController, animated: true)
     }
 }
