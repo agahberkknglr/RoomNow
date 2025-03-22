8feeec66ebaec057ca6a52d74bb36252087e9c5a
Agah Berkin Güler <yedekguler@gmail.com>
2025-03-22 19:56:44 +0300

    create destination vc

---
+++ RoomNow/Screens/Search/View/DestinationVC.swift
@@ -0,0 +1,19 @@
+//
+//  DestinationVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 22.03.2025.
+//
+
+import UIKit
+
+final class DestinationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
