ef8655551e82c7f5e42f01ac001d0f1715f78aa5
Agah Berkin Güler <yedekguler@gmail.com>
2025-03-27 10:58:02 +0300

    create room vc and update the view

---
+++ RoomNow/Screens/Search/View/RoomVC.swift
@@ -0,0 +1,137 @@
+//
+//  RoomVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 25.03.2025.
+//
+
+import UIKit
+
+protocol RoomVCProtocols {
+    func configureVC()
+    //func configureButtons()
+}
+
+class RoomVC: UIViewController, RoomVCProtocols {
+
+    private let titleLabel = UILabel()
+    private let roomLabel = UILabel()
+    private let adultsLabel = UILabel()
+    private let childrenLabel = UILabel()
+    private let childrenInfoLabel = UILabel()
+    private let petLabel = UILabel()
+    private let petInfoLabel = UILabel()
+    private let petSwitch = UISwitch()
+    
+    private let roomStackView = UIStackView()
+    private let adultsStackView = UIStackView()
+    private let childrenStackView = UIStackView()
+    private let childrenLabelsStackView = UIStackView()
+    private let petStackView = UIStackView()
+    
+    private let roomCounter = CounterView(startValue: 1, minValue: 1)
+    private let adultsCounter = CounterView(startValue: 2, minValue: 1)
+    private let childrenCounter = CounterView(startValue: 0)
+    
+    private let applyButton = UIButton()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureVC()
+
+    }
+    
+    func configureVC() {
+        view.backgroundColor = .secondarySystemBackground
+        
+        titleLabel.text = "Select rooms and guests"
+        titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        roomLabel.text = "Rooms"
+        adultsLabel.text = "Adults"
+        childrenLabel.text = "Children"
+        childrenInfoLabel.text = "0 - 17 years old"
+        childrenInfoLabel.font = .systemFont(ofSize: 14, weight: .light)
+        petLabel.text = "Travelling with pets?"
+        petInfoLabel.text = "Asistance animals aren't consider pets."
+        petInfoLabel.font = .systemFont(ofSize: 14, weight: .light)
+        
+        petSwitch.isOn = false
+        
+        roomStackView.axis = .horizontal
+        roomStackView.distribution = .fillProportionally
+        roomStackView.spacing = 10
+        roomStackView.addArrangedSubview(roomLabel)
+        roomStackView.addArrangedSubview(roomCounter)
+        roomStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        adultsStackView.axis = .horizontal
+        adultsStackView.distribution = .fillProportionally
+        adultsStackView.spacing = 10
+        adultsStackView.addArrangedSubview(adultsLabel)
+        adultsStackView.addArrangedSubview(adultsCounter)
+        adultsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        childrenLabelsStackView.axis = .vertical
+        childrenLabelsStackView.distribution = .fillProportionally
+        childrenLabelsStackView.spacing = 0
+        childrenLabelsStackView.addArrangedSubview(childrenLabel)
+        childrenLabelsStackView.addArrangedSubview(childrenInfoLabel)
+        childrenLabelsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        childrenStackView.axis = .horizontal
+        childrenStackView.distribution = .fillProportionally
+        childrenStackView.spacing = 10
+        childrenStackView.addArrangedSubview(childrenLabelsStackView)
+        childrenStackView.addArrangedSubview(childrenCounter)
+        childrenStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        petStackView.axis = .horizontal
+        petStackView.distribution = .fill
+        petStackView.spacing = 10
+        petStackView.addArrangedSubview(petLabel)
+        petStackView.addArrangedSubview(petSwitch)
+        petStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        let stackView: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [titleLabel, roomStackView, adultsStackView, childrenStackView, petStackView, petInfoLabel, applyButton])
+            stackView.axis = .vertical
+            stackView.distribution = .fillProportionally
+            stackView.spacing = 10
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            return stackView
+        }()
+        
+        applyButton.setTitle("Apply", for: .normal)
+        applyButton.layer.cornerRadius = 10
+        applyButton.tintColor = .white
+        applyButton.backgroundColor = .brown
+        
+
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 32),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            roomCounter.widthAnchor.constraint(equalToConstant: 100),
+            adultsCounter.widthAnchor.constraint(equalToConstant: 100),
+            childrenCounter.widthAnchor.constraint(equalToConstant: 100),
+            roomCounter.heightAnchor.constraint(equalToConstant: 50),
+            adultsCounter.heightAnchor.constraint(equalToConstant: 50),
+            childrenCounter.heightAnchor.constraint(equalToConstant: 50),
+            applyButton.widthAnchor.constraint(equalToConstant: 100),
+            applyButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+    }
+    
+
+}
+
+//extension RoomVC: RoomVCProtocols {
+//
+//}
+
