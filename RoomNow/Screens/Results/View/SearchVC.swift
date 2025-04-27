e0c80b52202d039a037baafe2a9802991e2d1259
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-27 17:42:59 +0300

    Create SearchVC.swift

---
+++ RoomNow/Screens/Results/View/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 27.04.2025.
+//
+
+import UIKit
+
+final class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
