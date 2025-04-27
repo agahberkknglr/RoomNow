5e2ed517cd0a450d8e9ede16f83e673692660bf4
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-27 17:46:29 +0300

    Create ResultVC.swift

---
+++ RoomNow/Screens/Results/View/ResultVC.swift
@@ -0,0 +1,19 @@
+//
+//  ResultVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 27.04.2025.
+//
+
+import UIKit
+
+final class ResultVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
