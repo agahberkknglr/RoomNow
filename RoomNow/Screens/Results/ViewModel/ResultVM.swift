d6c2906899e7eef4f20948305983ea9a61d95944
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-27 17:57:53 +0300

    Update ResultVM.swift

--- RoomNow/Screens/Results/ViewModel/ResultVM.swift
+++ RoomNow/Screens/Results/ViewModel/ResultVM.swift
@@ -5,6 +5,16 @@
 //  Created by Agah Berkin Güler on 27.04.2025.
 //
 
+protocol ResultVMProtocol{
+    func viewDidLoad()
+}
+
 final class ResultVM {
-    
+    weak var view: ResultVCProtocol?
+}
+
+extension ResultVM: ResultVMProtocol {
+    func viewDidLoad() {
+        view?.configureVC()
+    }
 }
