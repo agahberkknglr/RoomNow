dc581e9294574407b394b00bfccc439febeda6c6
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-29 00:41:32 +0300

    Update ResultVC.swift

--- RoomNow/Screens/Results/View/ResultVC.swift
+++ RoomNow/Screens/Results/View/ResultVC.swift
@@ -13,19 +13,33 @@ protocol ResultVCProtocol: AnyObject {
 
 final class ResultVC: UIViewController {
     
-    private let viewModel = ResultVM()
+    private let viewModel: ResultVM
 
+    init(searchParameters: HotelSearchParameters) {
+        self.viewModel = ResultVM(searchParameters: searchParameters)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.view = self
         viewModel.viewDidLoad()
     }
-
-
 }
 
 extension ResultVC: ResultVCProtocol {
     func configureVC() {
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        
+        viewModel.fetchHotels {
+            DispatchQueue.main.async {
+                print("Oteller geldi, CollectionView/TableView reload yapılacak.")
+                // burada reloadData çağıracağız (sonraki adımda)
+            }
+        }
     }
 }
