69fce3e1b026a967150613256a400bcc11b6f9a1
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-29 00:41:20 +0300

    Create Search.swift

---
+++ RoomNow/Models/Search.swift
@@ -0,0 +1,16 @@
+//
+//  Search.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 28.04.2025.
+//
+
+import Foundation
+
+struct HotelSearchParameters {
+    let destination: String
+    let checkInDate: Date
+    let checkOutDate: Date
+    let guestCount: Int
+    let roomCount: Int
+}
