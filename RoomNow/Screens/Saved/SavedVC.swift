29f4359dba4f83cd2de1a0592c590441b4d34389
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-29 08:24:02 +0300

    Create SavedVC.swift

---
+++ RoomNow/Screens/Saved/SavedVC.swift
@@ -0,0 +1,19 @@
+//
+//  SavedVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 29.04.2025.
+//
+
+import UIKit
+
+final class SavedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBrown
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
