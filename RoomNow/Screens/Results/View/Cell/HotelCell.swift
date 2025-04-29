08d13ea2b8bf88bf0517e20624d4a7604773e8e2
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-29 05:55:15 +0300

    visual update on the cell

--- RoomNow/Screens/Results/View/Cell/HotelCell.swift
+++ RoomNow/Screens/Results/View/Cell/HotelCell.swift
@@ -14,7 +14,9 @@ final class HotelCell: UICollectionViewCell {
     private let hotelImageView = UIImageView()
     private let hotelNameLabel = UILabel()
     private let ratingLabel = UILabel()
+    private let locationLabel = UILabel()
     private let priceLabel = UILabel()
+    private let infoLabel = UILabel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,25 +30,34 @@ final class HotelCell: UICollectionViewCell {
     private func setupUI() {
         contentView.layer.cornerRadius = 12
         contentView.layer.borderWidth = 1
-        contentView.layer.borderColor = UIColor.systemGray5.cgColor
+        contentView.layer.borderColor = UIColor.clear.cgColor
         contentView.clipsToBounds = true
         
         hotelImageView.contentMode = .scaleAspectFill
-        hotelImageView.tintColor = .systemBlue
-        hotelImageView.image = UIImage(systemName: "building.2") // system hotel image
+        hotelImageView.image = UIImage(named: "hotelph") // system hotel image
+        hotelImageView.layer.cornerRadius = 12
+        hotelImageView.clipsToBounds = true
         
         hotelNameLabel.font = .boldSystemFont(ofSize: 16)
         hotelNameLabel.numberOfLines = 2
         
         ratingLabel.font = .systemFont(ofSize: 14)
-        ratingLabel.textColor = .systemGray
+        ratingLabel.textColor = .white
+        
+        locationLabel.font = .systemFont(ofSize: 14)
+        locationLabel.textColor = .white
         
         priceLabel.font = .boldSystemFont(ofSize: 16)
-        priceLabel.textColor = .systemGreen
+        priceLabel.textColor = .white
+        priceLabel.textAlignment = .right
         
-        let stackView = UIStackView(arrangedSubviews: [hotelNameLabel, ratingLabel, priceLabel])
+        infoLabel.font = .systemFont(ofSize: 14)
+        infoLabel.textColor = .white
+        infoLabel.textAlignment = .right
+        
+        let stackView = UIStackView(arrangedSubviews: [hotelNameLabel, ratingLabel, locationLabel, priceLabel, infoLabel])
         stackView.axis = .vertical
-        stackView.spacing = 4
+        stackView.spacing = 8
         
         contentView.addSubview(hotelImageView)
         contentView.addSubview(stackView)
@@ -56,12 +67,12 @@ final class HotelCell: UICollectionViewCell {
         
         NSLayoutConstraint.activate([
             hotelImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            hotelImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            hotelImageView.widthAnchor.constraint(equalToConstant: 60),
-            hotelImageView.heightAnchor.constraint(equalToConstant: 60),
+            hotelImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: 8),
+            hotelImageView.widthAnchor.constraint(equalToConstant: 120),
+            hotelImageView.heightAnchor.constraint(equalToConstant: 230),
             
-            stackView.topAnchor.constraint(equalTo: hotelImageView.bottomAnchor, constant: 8),
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: hotelImageView.trailingAnchor, constant: 8),
             stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
             stackView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -8)
         ])
@@ -70,6 +81,7 @@ final class HotelCell: UICollectionViewCell {
     func configure(with hotel: Hotel) {
         hotelNameLabel.text = hotel.name
         ratingLabel.text = "⭐️ \(hotel.rating)"
+        locationLabel.text = "📍 \(hotel.location)"
         
         let allRooms = hotel.roomTypes.flatMap { $0.rooms }
         if let minPrice = allRooms.map({ $0.price }).min() {
@@ -77,5 +89,6 @@ final class HotelCell: UICollectionViewCell {
         } else {
             priceLabel.text = "-"
         }
+        infoLabel.text = "No prepayment needed"
     }
 }
