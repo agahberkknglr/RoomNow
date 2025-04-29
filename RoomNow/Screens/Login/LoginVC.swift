6710e3484ade8f89c8c485cf2a920ef283d0ecc4
Agah Berkin Güler <yedekguler@gmail.com>
2025-04-29 08:23:46 +0300

    Create LoginVC.swift

---
+++ RoomNow/Screens/Login/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 29.04.2025.
+//
+
+import UIKit
+
+final class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBrown
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
