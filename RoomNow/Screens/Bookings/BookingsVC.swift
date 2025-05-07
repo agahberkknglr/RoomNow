c38716d5230d8b1f37b43edb935ea20104299060
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-08 02:27:43 +0300

    Update BookingsVC.swift

--- RoomNow/Screens/Bookings/BookingsVC.swift
+++ RoomNow/Screens/Bookings/BookingsVC.swift
@@ -8,22 +8,12 @@
 import UIKit
 
 final class BookingsVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .appBackground
-        // Do any additional setup after loading the view.
+        
+        //FirebaseManager.shared.uploadHotelsToFirestore()
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
