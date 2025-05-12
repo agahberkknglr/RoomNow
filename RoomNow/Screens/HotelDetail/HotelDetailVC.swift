ea030c1fe3755f70ead47a204f63bb2de9a00e26
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-12 15:40:24 +0300

    Update HotelDetailVC.swift

--- RoomNow/Screens/HotelDetail/HotelDetailVC.swift
+++ RoomNow/Screens/HotelDetail/HotelDetailVC.swift
@@ -8,15 +8,30 @@
 import UIKit
 
 protocol HotelDetailVCProtocol: AnyObject {
-    
+    func configureVC()
 }
 
 final class HotelDetailVC: UIViewController {
-
     
+    private let viewModel: HotelDetailVMProtocol
+
+    init(viewModel: HotelDetailVMProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureVC()
+    }
+}
+
+extension HotelDetailVC: HotelDetailVCProtocol {
+    func configureVC() {
         view.backgroundColor = .appBackground
     }
 }
