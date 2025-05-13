4454ecd3a561afd8569a1eeaa6a1d017d0588141
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-13 23:45:13 +0300

    custom vertical divider created

---
+++ RoomNow/Utilities/UIComponents/VerticalDividerView.swift
@@ -0,0 +1,21 @@
+//
+//  VerticalDividerView.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 13.05.2025.
+//
+
+import UIKit
+
+final class VerticalDividerView: UIView {
+    init(width: CGFloat = 1.0, color: UIColor = .appDivider) {
+        super.init(frame: .zero)
+        backgroundColor = color
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
