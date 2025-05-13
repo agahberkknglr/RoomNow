67a48aef794b35bbea174211e21c7e2e6c20faa2
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-13 22:53:39 +0300

    UIView ext for pin the views to content view or set with insets

---
+++ RoomNow/Utilities/Extensions/UIView.swift
@@ -0,0 +1,21 @@
+//
+//  UIView.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 13.05.2025.
+//
+
+import UIKit
+
+extension UIView {
+    func pinToEdges(of superview: UIView, withInsets insets: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        superview.addSubview(self)
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor, constant: insets.top),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: insets.left),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -insets.right),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -insets.bottom)
+        ])
+    }
+}
