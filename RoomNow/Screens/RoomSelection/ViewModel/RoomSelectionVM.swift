7623268f2472895e30f4908ee4b85aaaa94cc771
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-16 22:12:45 +0300

    Update RoomSelectionVM.swift

--- RoomNow/Screens/RoomSelection/ViewModel/RoomSelectionVM.swift
+++ RoomNow/Screens/RoomSelection/ViewModel/RoomSelectionVM.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+protocol RoomSelectionVMProtocol {
+    var availableRooms: [RoomType] { get }
+    var hotelName: String { get }
+    func filterAvailableRooms()
+}
+
+final class RoomSelectionVM: RoomSelectionVMProtocol {
+    
+    private let hotel: Hotel
+    private let searchParams: HotelSearchParameters
+    
+    private(set) var availableRooms: [RoomType] = []
+    
+    var hotelName: String { hotel.name }
+    
+    init(hotel: Hotel, searchParams: HotelSearchParameters) {
+        self.hotel = hotel
+        self.searchParams = searchParams
+        filterAvailableRooms()
+    }
+    
+    func filterAvailableRooms() {
+        availableRooms = hotel.roomTypes.compactMap { type in
+            let filtered = type.rooms.filter {
+                $0.bedCapacity >= searchParams.guestCount &&
+                $0.isAvailable(for: searchParams.checkInDate, checkOut: searchParams.checkOutDate)
+            }
+            return filtered.isEmpty ? nil : RoomType(typeName: type.typeName, rooms: filtered)
+        }
+    }
+}
