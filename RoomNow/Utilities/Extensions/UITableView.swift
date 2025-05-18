e9c8e15f92b5c84a7f06480b6fe5dcec735d9fa8
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-18 18:54:37 +0300

    dequeue extension added

--- RoomNow/Utilities/Extensions/UITableView.swift
+++ RoomNow/Utilities/Extensions/UITableView.swift
@@ -11,4 +11,12 @@ extension UITableView {
     func registerCell<T: UITableViewCell>(type: T.Type) {
         register(type, forCellReuseIdentifier: String(describing: type))
     }
+    
+    func dequeue<T: UITableViewCell>(_ cellType: T.Type, for indexPath: IndexPath) -> T {
+        let identifier = String(describing: cellType)
+        guard let cell = dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(identifier)")
+        }
+        return cell
+    }
 }
