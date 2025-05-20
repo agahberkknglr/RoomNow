fd714173a625e19c69d5cb63f4e23042f84c8528
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-20 04:43:15 +0300

    Create AuthManager.swift

---
+++ RoomNow/Network/FirebaseManager/AuthManager.swift
@@ -0,0 +1,48 @@
+//
+//  AuthManager.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 20.05.2025.
+//
+
+import FirebaseAuth
+
+final class AuthManager {
+    static let shared = AuthManager()
+
+    private init() {}
+
+    func register(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { _, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+
+    func login(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { _, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+
+    func logout(completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            try Auth.auth().signOut()
+            completion(.success(()))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+    var currentUser: User? {
+        return Auth.auth().currentUser
+    }
+}
+
