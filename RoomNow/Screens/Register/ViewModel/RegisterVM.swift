124ea8802aa242efbb84952f4279d52a52a4bd2b
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-20 05:53:29 +0300

    Update RegisterVM.swift

--- RoomNow/Screens/Register/ViewModel/RegisterVM.swift
+++ RoomNow/Screens/Register/ViewModel/RegisterVM.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 protocol RegisterVMProtocol {
-    func register(email: String, password: String, completion: @escaping (String?) -> Void)
+    func register(email: String, password: String, username: String, dateOfBirth: String, gender: String, completion: @escaping (String?) -> Void)
 }
 
 final class RegisterVM: RegisterVMProtocol {
-    func register(email: String, password: String, completion: @escaping (String?) -> Void) {
-        AuthManager.shared.register(email: email, password: password) { result in
+    func register(email: String, password: String, username: String, dateOfBirth: String, gender: String, completion: @escaping (String?) -> Void) {
+        AuthManager.shared.register(email: email, password: password, username: username, dateOfBirth: dateOfBirth, gender: gender) { result in
             switch result {
             case .success:
                 completion(nil)
