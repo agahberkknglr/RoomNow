108374f08ce1dedcfc2425c16bc679e2bc530975
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-21 15:53:26 +0300

    saved hotel model to save search parameters to firebase

---
+++ RoomNow/Models/SavedHotel.swift
@@ -0,0 +1,19 @@
+//
+//  SavedHotel.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 21.05.2025.
+//
+
+import Foundation
+
+struct SavedHotel: Codable {
+    let hotelId: String
+    let hotelName: String
+    let savedAt: Date
+    let checkInDate: Date
+    let checkOutDate: Date
+    let guestCount: Int
+    let roomCount: Int
+    let selectedRoomNumber: String?
+}
