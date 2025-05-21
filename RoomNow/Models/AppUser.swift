c1a3efaef7d2909f3843732e7bf8706786d5dd56
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-21 11:03:10 +0300

    Create AppUser.swift

---
+++ RoomNow/Models/AppUser.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 20.05.2025.
+//
+
+import Foundation
+
+struct AppUser: Codable {
+    let uid: String
+    var username: String
+    var email: String
+    var gender: String
+    var dateOfBirth: String
+    var profileImageUrl: String?
+}
