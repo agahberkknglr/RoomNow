c3e22b1b0746cb2d364ce810b45f465dd72dab0f
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-22 07:14:56 +0300

    Create SavedHotelsVC.swift

---
+++ RoomNow/Screens/SavedHotels/View/SavedHotelsVC.swift
@@ -0,0 +1,18 @@
+//
+//  SavedHotelsVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 22.05.2025.
+//
+
+import UIKit
+
+final class SavedHotelsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .appBackground
+        
+        // Do any additional setup after loading the view.
+    }
+}
