c9cd3dc584fda0197a23e20f52d9e82c06aed9cb
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-22 07:14:52 +0300

    updated with fetch data and city name sorted

---
+++ RoomNow/Screens/Saved/ViewModel/SavedVM.swift
@@ -0,0 +1,34 @@
+//
+//  SavedVM.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 29.04.2025.
+//
+
+import Foundation
+
+final class SavedVM {
+    private(set) var cityHotelMap: [String: [SavedHotel]] = [:]
+
+    func fetchSavedData(completion: @escaping () -> Void) {
+        FirebaseManager.shared.fetchSavedHotels { [weak self] result in
+            switch result {
+            case .success(let hotels):
+                self?.cityHotelMap = Dictionary(grouping: hotels, by: { $0.city })
+            case .failure(let error):
+                print(" Error fetching saved hotels: \(error)")
+                self?.cityHotelMap = [:]
+            }
+            completion()
+        }
+    }
+
+    var cityNames: [String] {
+        Array(cityHotelMap.keys).sorted()
+    }
+
+    func hotels(for city: String) -> [SavedHotel] {
+        cityHotelMap[city] ?? []
+    }
+}
+
