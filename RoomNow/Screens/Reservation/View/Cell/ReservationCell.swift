f52620ce7a6f774cb832619784ed403304e0e2a8
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-23 20:45:28 +0300

    Create ReservationCell.swift

---
+++ RoomNow/Screens/Reservation/View/Cell/ReservationCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReservationCell.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 23.05.2025.
+//
+
+import UIKit
+
+class ReservationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
