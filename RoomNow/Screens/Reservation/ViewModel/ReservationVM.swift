afb8d4a9c9876d45fc0c500159ea5a62eb4bb506
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-23 20:45:26 +0300

    Create ReservationVM.swift

---
+++ RoomNow/Screens/Reservation/ViewModel/ReservationVM.swift
@@ -0,0 +1,46 @@
+//
+//  ReservationVM.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 29.04.2025.
+//
+
+import Foundation
+
+final class ReservationVM {
+    let hotel: Hotel
+    let searchParams: HotelSearchParameters
+    let selectedRooms: [HotelRoom]
+
+    let fullName: String
+    let email: String
+    let phone: String
+    let note: String?
+
+    init(
+        hotel: Hotel,
+        searchParams: HotelSearchParameters,
+        selectedRooms: [HotelRoom],
+        fullName: String,
+        email: String,
+        phone: String,
+        note: String?
+    ) {
+        self.hotel = hotel
+        self.searchParams = searchParams
+        self.selectedRooms = selectedRooms
+        self.fullName = fullName
+        self.email = email
+        self.phone = phone
+        self.note = note
+    }
+
+    var numberOfNights: Int {
+        let nights = Calendar.current.dateComponents([.day], from: searchParams.checkInDate, to: searchParams.checkOutDate).day ?? 1
+        return max(nights, 1)
+    }
+
+    var totalPrice: Int {
+        selectedRooms.reduce(0) { $0 + (Int($1.price) * numberOfNights) }
+    }
+}
