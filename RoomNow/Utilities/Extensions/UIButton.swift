2fb3c370460164a71c73324272ac98fcf7002f29
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-24 00:17:59 +0300

    UIButton extension

---
+++ RoomNow/Utilities/Extensions/UIButton.swift
@@ -0,0 +1,32 @@
+//
+//  UIButton.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 23.05.2025.
+//
+
+import UIKit
+
+extension UIButton {
+    func applyPrimaryStyle(with title: String) {
+        self.backgroundColor = UIColor.appButtonBackground
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(.appAccent, for: .normal)
+        self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        self.layer.cornerRadius = 8
+        self.clipsToBounds = true
+    }
+    
+    func applyLogOutStyle() {
+        self.backgroundColor = UIColor.appButtonBackground
+        self.setTitleColor(.appError, for: .normal)
+        self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        self.layer.cornerRadius = 8
+        self.clipsToBounds = true
+    }
+
+    func setEnabled(_ isEnabled: Bool) {
+        self.isEnabled = isEnabled
+        self.alpha = isEnabled ? 1.0 : 0.5
+    }
+}
