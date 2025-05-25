58f8fa7e847f3dee0f32e5344105782143a590e9
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-26 02:41:29 +0300

    booking detail cell created

---
+++ RoomNow/Screens/BookingDetail/View/Cell/BookingPriceInfoCell.swift
@@ -0,0 +1,64 @@
+//
+//  BookingPriceInfoCell.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 26.05.2025.
+//
+
+import UIKit
+
+final class BookingPriceInfoCell: UITableViewCell {
+    
+    private let priceLabel = UILabel()
+    private let nightLabel = UILabel()
+    private let view: UIView = {
+        let view = UIView()
+        view.backgroundColor = .appSecondaryBackground
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 12
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupUI() {
+        contentView.backgroundColor = .appBackground
+
+        priceLabel.font = .boldSystemFont(ofSize: 18)
+        priceLabel.textColor = .appPrimaryText
+        priceLabel.textAlignment = .left
+
+        nightLabel.font = .systemFont(ofSize: 14)
+        nightLabel.textColor = .appSecondaryText
+        nightLabel.textAlignment = .left
+
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        nightLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.addSubview(view)
+        view.addSubview(priceLabel)
+        view.addSubview(nightLabel)
+        view.pinToEdges(of: contentView, withInsets: UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16))
+        
+        NSLayoutConstraint.activate([
+            nightLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            nightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            nightLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            priceLabel.topAnchor.constraint(equalTo: nightLabel.bottomAnchor, constant: 4),
+            priceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            priceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            priceLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12)
+        ])
+    }
+    
+    func configure() {
+        
+    }
+}
---
+++ RoomNow/Screens/BookingDetail/View/Cell/BookingRoomInfoCell.swift
@@ -0,0 +1,72 @@
+//
+//  BookingRoomInfoCell.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 26.05.2025.
+//
+
+import UIKit
+
+final class BookingRoomInfoCell: UITableViewCell {
+    
+    private let roomTypeLabel = UILabel()
+    private let roomInfoLabel = UILabel()
+    private let roomUserNameLabel = UILabel()
+    private let view: UIView = {
+        let view = UIView()
+        view.backgroundColor = .appSecondaryBackground
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 12
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupUI(){
+        contentView.backgroundColor = .appBackground
+
+        roomTypeLabel.font = .boldSystemFont(ofSize: 16)
+        roomTypeLabel.textColor = .appPrimaryText
+
+        roomInfoLabel.font = .systemFont(ofSize: 14)
+        roomInfoLabel.textColor = .appSecondaryText
+
+        roomUserNameLabel.font = .systemFont(ofSize: 14)
+        roomUserNameLabel.textColor = .appAccent
+
+        roomTypeLabel.translatesAutoresizingMaskIntoConstraints = false
+        roomInfoLabel.translatesAutoresizingMaskIntoConstraints = false
+        roomUserNameLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.addSubview(view)
+        view.addSubview(roomTypeLabel)
+        view.addSubview(roomInfoLabel)
+        view.addSubview(roomUserNameLabel)
+        view.pinToEdges(of: contentView, withInsets: UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16))
+        
+        NSLayoutConstraint.activate([
+            roomTypeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            roomTypeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            roomTypeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+
+            roomInfoLabel.topAnchor.constraint(equalTo: roomTypeLabel.bottomAnchor, constant: 4),
+            roomInfoLabel.leadingAnchor.constraint(equalTo: roomTypeLabel.leadingAnchor),
+            roomInfoLabel.trailingAnchor.constraint(equalTo: roomTypeLabel.trailingAnchor),
+
+            roomUserNameLabel.topAnchor.constraint(equalTo: roomInfoLabel.bottomAnchor, constant: 4),
+            roomUserNameLabel.leadingAnchor.constraint(equalTo: roomTypeLabel.leadingAnchor),
+            roomUserNameLabel.trailingAnchor.constraint(equalTo: roomTypeLabel.trailingAnchor),
+            roomUserNameLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12)
+        ])
+    }
+    
+    func configure(){
+        
+    }
+}
