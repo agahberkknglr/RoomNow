205a07de3aad53b2189fd3bdff43b0d5e3a2dd94
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-26 16:50:29 +0300

    history cell vm

---
+++ RoomNow/Screens/Search/ViewModel/HistoryCellVM.swift
@@ -0,0 +1,12 @@
+//
+//  HistoryCellVM.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 26.05.2025.
+//
+
+struct HistoryCellViewModel {
+    let destination: String
+    let dateRange: String
+    let guestSummary: String
+}
