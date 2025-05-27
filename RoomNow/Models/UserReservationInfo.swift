77034e43b04860f9a142bc899b955cdaf7da4a7b
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-27 20:01:58 +0300

    Create UserReservationInfo.swift

---
+++ RoomNow/Models/UserReservationInfo.swift
@@ -0,0 +1,13 @@
+//
+//  UserReservationInfo.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 27.05.2025.
+//
+
+struct UserReservationInfo {
+    var name: String?
+    var email: String?
+    var phone: String?
+    var note: String?
+}
