a27a24fe0e9fc221296bb8c7fa890be18c14a65d
Agah Berkin Güler <yedekguler@gmail.com>
2025-05-28 18:52:53 +0300

    Update ReservationStatus.swift

--- RoomNow/Utilities/Enums/ReservationStatus.swift
+++ RoomNow/Utilities/Enums/ReservationStatus.swift
@@ -6,9 +6,31 @@
 //  Created by Agah Berkin Güler on 25.05.2025.
 //
 
+import UIKit
+
 enum ReservationStatus: String, Codable {
     case active
     case ongoing
     case cancelled
     case completed
+    
+    var description: String {
+        switch self {
+        case .active, .ongoing:
+            return "Reservations can be canceled up to 1 day before check-in."
+        case .completed:
+            return "This reservation has been completed."
+        case .cancelled:
+            return "This reservation was cancelled."
+        }
+    }
+
+    var color: UIColor {
+        switch self {
+        case .active: return .appSuccess
+        case .ongoing: return .appWarning
+        case .completed: return .appSecondaryText
+        case .cancelled: return .appError
+        }
+    }
 }
