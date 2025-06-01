29cf3bfaf55aa662711e86cbae59349ffe9573dd
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-01 22:10:49 +0300

    admin panel created

---
+++ RoomNow/Screens/AdminDashboard/View/AdminDashboardVC.swift
@@ -0,0 +1,17 @@
+//
+//  AdminDashboardVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 1.06.2025.
+//
+
+import UIKit
+
+final class AdminDashboardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ RoomNow/Screens/AdminDashboard/ViewModel/AdminDashboardVM.swift
@@ -0,0 +1,10 @@
+//
+//  AdminDashboardVM.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 1.06.2025.
+//
+
+final class AdminDashboardVM {
+    
+}
