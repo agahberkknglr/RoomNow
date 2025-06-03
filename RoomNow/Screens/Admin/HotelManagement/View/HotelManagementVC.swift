66c1fa45bb96a65d281672c43249388481e140d8
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-03 23:00:39 +0300

    Create HotelManagementVC.swift

---
+++ RoomNow/Screens/Admin/HotelManagement/View/HotelManagementVC.swift
@@ -0,0 +1,18 @@
+//
+//  HotelManagementVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 3.06.2025.
+//
+
+import UIKit
+
+final class HotelManagementVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
