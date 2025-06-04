1bf49146f03f9dc44ac5c5ed1248cdb9756e0900
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-04 23:08:06 +0300

    Update AdminDashboardVM.swift

--- RoomNow/Screens/Admin/AdminDashboard/ViewModel/AdminDashboardVM.swift
+++ RoomNow/Screens/Admin/AdminDashboard/ViewModel/AdminDashboardVM.swift
@@ -5,6 +5,16 @@
 //  Created by Agah Berkin Güler on 1.06.2025.
 //
 
+import Foundation
+
+
 final class AdminDashboardVM {
-    
+
+    var hotels: [Hotel] = []
+    var didUpdate: (() -> Void)?
+    var adminName: String = "Admin" // Replace with actual name if needed
+
+    func fetchHotels() {
+
+    }
 }
