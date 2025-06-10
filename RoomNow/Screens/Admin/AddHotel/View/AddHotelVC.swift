029516ec658cb3037317c8d15b3a844a7e98ec18
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-10 17:32:09 +0300

    Create AddHotelVC.swift

---
+++ RoomNow/Screens/Admin/AddHotel/View/AddHotelVC.swift
@@ -0,0 +1,20 @@
+//
+//  AddHotelVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 10.06.2025.
+//
+
+import UIKit
+
+final class AddHotelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .appBackground
+    }
+}
