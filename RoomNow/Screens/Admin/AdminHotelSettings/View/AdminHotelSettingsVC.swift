080be1386fe8f531406a020684019e37dfeaac8a
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-10 17:36:22 +0300

    Create AdminHotelSettingsVC.swift

---
+++ RoomNow/Screens/Admin/AdminHotelSettings/View/AdminHotelSettingsVC.swift
@@ -0,0 +1,20 @@
+//
+//  AdminHotelSettingsVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 10.06.2025.
+//
+
+import UIKit
+
+final class AdminHotelSettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .appBackground
+    }
+}
