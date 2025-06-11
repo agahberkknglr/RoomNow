af1501e5616a227bf896d97a31b3781b775e137c
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-12 00:31:50 +0300

    image data extension

--- RoomNow/Utilities/Extensions/String.swift
+++ RoomNow/Utilities/Extensions/String.swift
@@ -5,11 +5,18 @@
 //  Created by Agah Berkin Güler on 29.05.2025.
 //
 
-import Foundation
+import UIKit
 
 extension String {
     var isValidEmail: Bool {
         let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: self)
+    }
+}
+
+extension String {
+    var asBase64Image: UIImage? {
+        guard let data = Data(base64Encoded: self) else { return nil }
+        return UIImage(data: data)
     }
 }
---
+++ RoomNow/Utilities/Extensions/UIImageView.swift
@@ -0,0 +1,19 @@
+//
+//  UIImageView.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 12.06.2025.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImage(fromBase64 base64String: String?) {
+        if let base64 = base64String,
+           let image = base64.asBase64Image {
+            self.image = image
+        } else {
+            self.image = UIImage(systemName: "building.2.fill")
+        }
+    }
+}
