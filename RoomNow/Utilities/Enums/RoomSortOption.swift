917ef1931440a88ae269587c77641ff9676b14ee
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-13 02:53:13 +0300

    Create RoomSortOption.swift

---
+++ RoomNow/Utilities/Enums/RoomSortOption.swift
@@ -0,0 +1,13 @@
+//
+//  RoomSortOption.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 13.06.2025.
+//
+
+enum RoomSortOption: String, CaseIterable {
+    case priceLowToHigh = "Price ↑"
+    case priceHighToLow = "Price ↓"
+    case roomNumber = "Room Number"
+    case bedCount = "Bed Count"
+}
