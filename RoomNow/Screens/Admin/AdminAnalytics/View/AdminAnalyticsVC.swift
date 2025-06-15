629e7f3c4b1d592ab4a8579a9f9997855c45030e
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-15 18:02:09 +0300

    Create AdminAnalyticsVC.swift

---
+++ RoomNow/Screens/Admin/AdminAnalytics/View/AdminAnalyticsVC.swift
@@ -0,0 +1,18 @@
+//
+//  AdminAnalyticsVC.swift
+//  RoomNow
+//
+//  Created by Agah Berkin Güler on 15.06.2025.
+//
+
+import UIKit
+
+final class AdminAnalyticsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
