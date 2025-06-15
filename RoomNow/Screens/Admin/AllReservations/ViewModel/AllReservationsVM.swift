75e334876a71f87d2e930546a9af5dc159bf484c
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-15 04:11:01 +0300

    Update AllReservationsVM.swift

--- RoomNow/Screens/Admin/AllReservations/ViewModel/AllReservationsVM.swift
+++ RoomNow/Screens/Admin/AllReservations/ViewModel/AllReservationsVM.swift
@@ -9,17 +9,35 @@ import Foundation
 
 protocol AllReservationsVMProtocol {
     var reservations: [AdminReservation] { get }
+    var allReservations: [AdminReservation] { get }
+    var currentFilter: ReservationFilter { get set }
     func fetchReservations(completion: @escaping () -> Void)
 }
 
 final class AllReservationsVM: AllReservationsVMProtocol {
     private(set) var reservations: [AdminReservation] = []
+    private(set) var allReservations: [AdminReservation] = []
+
+    var currentFilter = ReservationFilter() {
+        didSet { applyFilter() }
+    }
 
     func fetchReservations(completion: @escaping () -> Void) {
-        FirebaseManager.shared.fetchAllReservations { [weak self] reservations in
-            self?.reservations = reservations
+        FirebaseManager.shared.fetchAllReservations { [weak self] fetched in
+            self?.allReservations = fetched
+            self?.applyFilter()
             completion()
         }
     }
+
+    private func applyFilter() {
+        if currentFilter.isEmpty {
+            reservations = allReservations
+        } else {
+            reservations = allReservations.filter { currentFilter.matches($0) }
+        }
+    }
 }
+
+
 
