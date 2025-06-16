cd1e4c2b855aac57bb85c48085fea3a8e41a9c0a
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-16 17:41:12 +0300

    Update UIImageView.swift

--- RoomNow/Utilities/Extensions/UIImageView.swift
+++ RoomNow/Utilities/Extensions/UIImageView.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 extension UIImageView {
-    func setImage(fromBase64 base64String: String?) {
+    func setImage(fromBase64 base64String: String?, placeholder: String = "building.2.fill") {
         if let base64 = base64String,
            let image = base64.asBase64Image {
             self.image = image
         } else {
-            self.image = UIImage(systemName: "building.2.fill")
+            self.image = UIImage(systemName: placeholder)
         }
     }
 }
