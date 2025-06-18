8dd715e1b3e64123111c0afded7982048fe0db97
Agah Berkin Güler <yedekguler@gmail.com>
2025-06-18 07:36:57 +0300

    Update OnboardingVM.swift

--- RoomNow/Screens/Shared/Onboarding/ViewModel/OnboardingVM.swift
+++ RoomNow/Screens/Shared/Onboarding/ViewModel/OnboardingVM.swift
@@ -7,9 +7,9 @@
 
 final class OnboardingVM {
     let pages: [OnboardingPage] = [
-        OnboardingPage(title: "Welcome to RoomNow", subtitle: "Your personal hotel booking assistant.", imageName: "onboardingph"),
-        OnboardingPage(title: "Search Smarter", subtitle: "Find hotels based on destination, date, and guests in seconds.", imageName: "onboardingph"),
-        OnboardingPage(title: "Save & Book", subtitle: "Bookmark your favorite hotels and make secure bookings.", imageName: "onboardingph"),
-        OnboardingPage(title: "Chat-Based Booking", subtitle: "Let our chatbot help you to complete your reservation.", imageName: "onboardingph")
+        OnboardingPage(title: "Welcome to RoomNow", subtitle: "Your personal hotel booking assistant.", imageName: "ob1"),
+        OnboardingPage(title: "Search Smarter", subtitle: "Find hotels based on destination, date, and guests in seconds.", imageName: "ob2"),
+        OnboardingPage(title: "Save & Book", subtitle: "Bookmark your favorite hotels and make secure bookings.", imageName: "ob3"),
+        OnboardingPage(title: "Chat-Based Booking", subtitle: "Let our chatbot help you to complete your reservation.", imageName: "ob4")
     ]
 }
